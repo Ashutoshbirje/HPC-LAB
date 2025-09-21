a3540a492f46875ec29c12d755d17493cac42460
Ashutoshbirje <ashutoshbirje4@gmail.com>
2025-09-21 21:35:55 +0530

    ok

--- Assignment_No_6/01.c
+++ Assignment_No_6/01.c
@@ -4,9 +4,9 @@
 int main(int argc, char* argv[]) {
     int rank, size;
 
-    MPI_Init(&argc, &argv);                  // Initialize MPI
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);    // Get process rank
-    MPI_Comm_size(MPI_COMM_WORLD, &size);    // Get total processes
+    MPI_Init(&argc, &argv);                  
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);    
+    MPI_Comm_size(MPI_COMM_WORLD, &size);   
 
     if (size != 10) {
         if (rank == 0) {
